2cdf66c82f07cc22b63c4a29a7cd257eae95fd7e
dorpiee <76785022+ScorpGaming4432@users.noreply.github.com>
2026-02-16 00:35:18 +0100

    Improve debug logging footprint controls

--- debug.cpp
+++ debug.cpp
@@ -1,21 +1,4 @@
 #include "debug.h"
 
-const char* LOCATION_STRINGS[] = {
-  "SETUP", "LOOP", "TANK", "AM", "THRESH",
-  "PUMPS", "WATER", "CHARS", "TIME", "DUR", "STORAGE",
-  "ERROR", "NOT_IMPLEMENTED"
-};
-
-const char* ERROR_STRINGS[] = {
-  "not set", "not set correctly", "Sensor Failure", "Pump Failure", "Unknown Error"
-};
-
-template<typename... Args>
-void SerialPrint(Location tag, Args... args) {
-  Serial.print("[");
-  Serial.print(LOCATION_STRINGS[tag]);
-  Serial.print("] ");
-
-  (Serial.print(args), ...);
-  Serial.println();
-}
+// Intentionally left blank.
+// Debug helpers are header-only to support templated SerialPrint.
